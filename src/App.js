17f69af36cdda04cf910a7dfc10a190e765ff585
devMarilia <devMarilia.frontend@gmail.com>
2020-11-05 10:10:58 -0300

    Diagramação da tela APP.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
-
+import React, { Fragment } from 'react';
+import axios from 'axios';
 function App() {
   return (
-    <div className="App">
-      Previsão do tempo ReactJs
-    </div>
+    <Fragment>
+      <h3>Clima nas suas Coordenadas (Exemplo)</h3>
+      <hr />
+      <ul>
+        <li>Temperatura atual: x°</li>
+        <li>Temperatura máxima: x°</li>
+        <li>Temperatura minima: x°</li>
+        <li>Pressão: x hpa</li>
+        <li>Umidade: x%</li>
+      </ul>
+    </Fragment>
   );
 }
-
 export default App;
